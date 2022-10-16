ff277416ca9d22aaa560a844195404db40ce804d
Aashna Ahuja <49040152+aashu1328@users.noreply.github.com>
2022-10-16 17:27:04 -0500

    Add files via upload

--- Code.R
+++ Code.R
@@ -3,11 +3,15 @@ library(tidyverse)
 library(ggplot2)
 library(reshape2)
 library(glmnet)
+library(randomForest)
+library(caret)
+library(ggcorrplot)
 ########################### DATA IMPORT ##########################
 BodyFat <- read.csv("D:/Aashna/STAT 628/Module 2/BodyFat_Clean.csv")
 head(BodyFat)
-df<-subset(BodyFat,select = -c(IDNO,DENSITY,ADIPOSITY))
+df<-subset(BodyFat,select = -c(IDNO,DENSITY,ADIPOSITY,X))
 head(df)
+attach(df)
 ########################### CORRELATION CHECK ####################
 corr<-round(cor(df),3)
 head(corr)
@@ -21,7 +25,7 @@ ggplot(data = melted_cor, aes(x=Var1, y=Var2, fill=value)) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         panel.background = element_blank())
-plot(df)
+#plot(df)
 ##################### CORRELATION AFTER SCALING #################
 df3 <- df %>% mutate_all(~(scale(.) %>% as.vector))
 head(df3)
@@ -37,6 +41,9 @@ ggplot(data = melted_cor2, aes(x=Var1, y=Var2, fill=value)) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         panel.background = element_blank())
+
+ggcorrplot(corr, hc.order = TRUE, type = "lower",
+           lab = TRUE)
 ########################## BASIC MODELS ##########################
 model1<-lm(BODYFAT~.,data=df)
 summary(model1)
@@ -45,29 +52,55 @@ summary(model1)
 df2<-subset(BodyFat,select = -c(IDNO,DENSITY,HEIGHT,WEIGHT))
 model3<-lm(BODYFAT~.,data=df2)
 summary(model3)
-model4<-lm(BODYFAT~AGE+WEIGHT+HEIGHT,data=df)
-summary(model4)
-model5<-lm(BODYFAT~AGE+NECK+ABDOMEN+HIP+FOREARM+WRIST,data=df)
+model5<-lm(BODYFAT~AGE+NECK+ABDOMEN+FOREARM+WRIST,data=df) # highest accuracy
 summary(model5)
-model4<-lm(BODYFAT~WEIGHT+HEIGHT,data=df)
-summary(model4)
-############################ LASSO & RIDGE #######################
+model6<-lm(BODYFAT~WEIGHT+ABDOMEN,data=df)
+summary(model6)
+model7<-lm(BODYFAT~AGE +HEIGHT+WEIGHT,data=BodyFat)
+summary(model7)
+############################ LASSO & RF ########################################
 X<-as.matrix(df[,-1])
 Y<-as.matrix(df[1])
 set.seed(100)
 cv.lasso <- cv.glmnet(X, Y, alpha=1, parallel=TRUE, standardize=TRUE, type.measure='auc')
 cv.lasso <- cv.glmnet(X, Y)
-plot(cv.lasso)
 df_coef <- round(as.matrix(coef(cv.lasso, s=cv.lasso$lambda.min)), 2)
 df_coef[df_coef[, 1] != 0, ]
-cv.ridge<-cv.glmnet(X,Y,aplha=0)
-df_coef2 <- round(as.matrix(coef(cv.ridge, s=cv.ridge$lambda.min)), 2)
-df_coef2[df_coef2[, 1] != 0, ]
+fit_rf = randomForest(BODYFAT~., data=df) # Create an importance based on mean decreasing gini
+importance(fit_rf)
+######################## BODYFAT VS TOP 3 VARIABLES ###############
+par(mfrow=c(2,2))
+
+model_age<-lm(BODYFAT~AGE,data=df)
+plot(df$AGE,df$BODYFAT,xlab = "Age",ylab="BodyFat")
+#abline(model_age,col="red",lwd=3)
+title("Bodyfat vs Age")
+
+model_height<-lm(BODYFAT~HEIGHT,data=df)
+plot(df$HEIGHT,df$BODYFAT,xlab = "Height",ylab="BodyFat")
+#abline(model_height,col="blue",lwd=3)
+title("Bodyfat vs Height")
+
+model_ab<-lm(BODYFAT~ABDOMEN,data=df)
+plot(df$ABDOMEN,df$BODYFAT,xlab = "Abdomen",ylab="BodyFat")
+#abline(model_ab,col="green",lwd=3)
+title("Bodyfat vs Abdomen")
+##############################
+par(mfrow=c(2,2))
+par(mgp=c(1.8,.5,0), mar=c(3,3,1,1)) 
+hist(BODYFAT,breaks=30,cex.lab=1.5,cex.main=1.5,
+     main="Histogram of Body Fat %",xlab="Body Fat %")
+hist(AGE,breaks=30,cex.lab=1.5,cex.main=1.5,
+     main="Histogram of Age",xlab="Age (yrs)")
+hist(HEIGHT,breaks=30,cex.lab=1.5,cex.main=1.5,
+     main="Histogram of Height ",xlab="Height (cm)")
+hist(ABDOMEN,breaks=30,cex.lab=1.5,cex.main=1.5,
+     main="Histogram ofAbdomen",xlab="ABDOMEN (cm)")
 ###################### TOP MODEL COMPARISONS #####################
-model<-lm(BODYFAT~AGE+ABDOMEN+HEIGHT,data=BodyFat) # 3rd
-summary(model)
-model<-lm(BODYFAT~ABDOMEN+HEIGHT,data=BodyFat) # 1st
-summary(model)
+model_3<-lm(BODYFAT~AGE+ABDOMEN+HEIGHT,data=BodyFat) # 3rd
+summary(model_3)
+model_1<-lm(BODYFAT~ABDOMEN+HEIGHT,data=BodyFat) # 1st
+summary(model_1)
 plot(model)
-model<-lm(BODYFAT~ABDOMEN,data=BodyFat) # 2nd
-summary(model)
+model_2<-lm(BODYFAT~ABDOMEN,data=BodyFat) # 2nd
+summary(model_2)
